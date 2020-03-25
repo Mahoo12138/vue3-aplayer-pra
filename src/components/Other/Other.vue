5407821de18ed5f082f799dc98690999ea86ec57
mahoo12138 <mahoo12138@qq.com>
2020-03-25 15:13:18 +0800

    add weather api

--- src/components/Other/Other.vue
+++ src/components/Other/Other.vue
@@ -1,5 +1,7 @@
 <template>
-    <p>asdasd</p>
+   <v-container>
+       <v-btn color="red" @click="getWeather">test</v-btn>
+   </v-container>
 </template>
 
 <script>
@@ -12,36 +14,39 @@
           weather: {}
         }
     },
-    methods:{
-      getWeather(){
-        this.getLocation()
-        request({
-          method: 'post',
-          data: {
-            ip: this.ip,
-            key: 'f3209455c6a1ddb8792fee0bfed790a1'
-          },
-          url: '/ip/ipNew',
-        })
-      },
-      getLocation(){
-        request({
-          method: 'post',
-          data: {
-            ip: this.ip,
-            key: 'f3209455c6a1ddb8792fee0bfed790a1'
-          },
-          url: '/ip/ipNew',
-        }).then((res)=>{
-          console.log(res)
-        })
-        }
+    computed: {
+      getIPUrl(){
+        return '/ip/ipNew?ip='+ this.ip + '&key=f3209455c6a1ddb8792fee0bfed790a1'
       },
 
     },
-    created() {
-      console.log(this.ip)
-    }
+    methods:{
+      getWeather(){
+        request({
+          method: 'get',
+          url: this.getIPUrl,
+        }).then((res)=>{
+          // console.log(res.data.result)
+          let city = res.data.result.City
+          return request({
+            method: 'get',
+            url: this.getWUrl(city)
+          })
+        }).then(res => {
+          console.log(res)
+          this.weather = res.data.result
+        })
+
+
+
+      },
+      getWUrl(city){
+        let key = '市'
+        console.log("city_:"+city)
+        let index = city.indexOf(key)
+        return '/simpleWeather/query?city=' + city.substring(0,index) +'&key=b248d173e67960b90e422e2d16cd5f9c'
+      }
+    },
   }
 
 
